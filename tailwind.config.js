ac66747f342b19ab3fcde85698ba41d762402b0f
d.fathi <d.fathi@kian.digital>
2024-07-16 00:46:31 +0330

    customize tailwind and add some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+const withOpacity = (variableName) => {
+  console.log(variableName, "hello")
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) return `rgba(${variableName},${opacityValue})`
+
+    return `rgb(${variableName})`
+  }
+}
+
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: withOpacity("63, 162, 246"),
+        bg_light: withOpacity("241,245,249"),
+        text_bold: withOpacity("(33,33,33)"),
+        text_light: withOpacity("(130,130,130)"),
+      }
+    },
   },
   plugins: [],
 }
