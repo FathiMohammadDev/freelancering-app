7b5c23cc106d149a55c0f8986952bebe8be77028
d.fathi <d.fathi@kian.digital>
2024-07-18 14:36:46 +0330

    add return to auth service Functions

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,9 @@
 import http from "./httpService"
 
 export const getOTP = (data) => {
-    http.post("/user/get-otp",data)
+    return http.post("/user/get-otp", data)
 }
 
 export const checkOTP = (data) => {
-    http.post("/user/check-otp",data)
+   return http.post("/user/check-otp", data)
 }
