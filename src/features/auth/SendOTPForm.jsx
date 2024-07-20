a8ff000ad13bc2bed309317092358f8128937756
d.fathi <d.fathi@kian.digital>
2024-07-20 09:33:27 +0330

    handle send OTP

--- src/features/auth/SendOTPForm.jsx
+++ src/features/auth/SendOTPForm.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import { useMutation } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { getOTP } from "../../services/authService";
 
 const SendOTPForm = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const { data, error, isPending, mutateAsync } = useMutation({
+    mutationFn: getOTP,
+  });
+
+  const sendOtpHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await mutateAsync({ phoneNumber });
+      console.log(data.data.message);
+    } catch (error) {
+      console.log(error);
+      console.log("error");
+    }
+  };
+
   return (
-    <div className="w-full sm:max-w-sm mx-auto p-5">
-      <form className="space-y-4">
+    <div>
+      <form className="space-y-4" onSubmit={(e) => sendOtpHandler(e)}>
         <div>
           <label className="block text-text_light mb-1">Phone number:</label>
           <input
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
             type="text"
             className="w-full bg-bg_light p-2 outline-0 rounded-lg text-text_light"
           />
         </div>
         <button
           type="sumbit"
-          className="bg-primary text-white font-bold p-2 rounded-lg"
+          className="bg-primary text-white font-bold p-2 rounded-lg disabled:opacity-30"
+          disabled={isPending && true}
         >
           Send Code
         </button>
