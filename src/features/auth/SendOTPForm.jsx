c79360f5767e525f923740c9d90a046139c065c5
d.fathi <d.fathi@kian.digital>
2024-07-21 17:30:16 +0330

    change style in SendOTPForm

--- src/features/auth/SendOTPForm.jsx
+++ src/features/auth/SendOTPForm.jsx
@@ -10,10 +10,17 @@ const SendOTPForm = ({
   };
 
   return (
-    <div>
+    <div className="text-center">
+      <h1 className="text-xl font-medium text-text_bold">Create account</h1>
+      <p className="text-text_light text-sm py-2">
+        Please type your mobile number to
+        <br /> send OTP code
+      </p>
       <form className="space-y-4" onSubmit={(e) => sumbitFormHandler(e)}>
         <div>
-          <label className="block text-text_light mb-1">Phone number:</label>
+          <label className="text-left block text-text_light mb-1">
+            Phone number:
+          </label>
           <input
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
@@ -23,7 +30,7 @@ const SendOTPForm = ({
         </div>
         <button
           type="sumbit"
-          className="bg-primary text-white font-bold p-2 rounded-lg disabled:opacity-30"
+          className="bg-primary text-white w-full font-bold p-2 rounded-lg disabled:opacity-30"
           disabled={isPending && true}
         >
           Send Code
