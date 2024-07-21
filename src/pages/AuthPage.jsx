320452886e7dce3238572565798df92a4737aad8
d.fathi <d.fathi@kian.digital>
2024-07-21 23:41:56 +0330

    create pages folder and AuthPage and CompleteFormPage in

---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,11 @@
+import AuthContainer from "../features/auth/AuthContainer";
+
+const AuthPage = () => {
+  return (
+    <div className="w-full sm:max-w-sm mx-auto p-5">
+      <AuthContainer />
+    </div>
+  );
+};
+
+export default AuthPage;
---
+++ src/pages/CompleteProfilePage.jsx
@@ -0,0 +1,11 @@
+import CompleteProfileForm from "../features/auth/CompleteProfileForm"
+
+const CompleteProfilePage = () => {
+  return (
+    <div className="w-full sm:max-w-sm mx-auto p-5">
+      <CompleteProfileForm />
+    </div>
+  );
+};
+
+export default CompleteProfilePage;
