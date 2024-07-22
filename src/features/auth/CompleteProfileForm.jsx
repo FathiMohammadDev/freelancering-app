03acdde0df58f5898ccab3807b0f94aac884f1aa
d.fathi <d.fathi@kian.digital>
2024-07-22 08:49:31 +0330

    handle form submit in completeProfileForm

--- src/features/auth/CompleteProfileForm.jsx
+++ src/features/auth/CompleteProfileForm.jsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
+import { completeProfile } from "../../services/authService";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
 
 const CompleteProfileForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("");
+
+  const { data, isPending, error, mutateAsync } = useMutation({
+    mutationFn: completeProfile,
+  });
+
+  const sumbitFormHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await mutateAsync({ name, email, role });
+      toast.success(data.data.message);
+      console.log(data);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+      console.log(error);
+    }
+  };
 
   return (
     <div className="text-center">
@@ -12,7 +31,7 @@ const CompleteProfileForm = () => {
         Please type your mobile number to
         <br /> send OTP code
       </p>
-      <form className="space-y-4">
+      <form className="space-y-4" onSubmit={(e) => sumbitFormHandler(e)}>
         <div>
           <label className="text-left block text-text_light mb-1">Name:</label>
           <input
@@ -59,7 +78,8 @@ const CompleteProfileForm = () => {
         </div>
         <button
           type="sumbit"
-          className="bg-primary text-white w-full font-bold p-2 rounded-lg disabled:opacity-30"
+          disabled={isPending && true}
+          className="bg-primary text-white w-full font-bold p-2 rounded-lg disabled:opacity-30 disabled:opacity-30"
         >
           Send Code
         </button>
