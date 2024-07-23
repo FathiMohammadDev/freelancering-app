67026179c724f1eb75032b55929db1ff5ae61fd9
d.fathi <d.fathi@kian.digital>
2024-07-23 18:34:07 +0330

    Setting up react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthContainer from "./features/auth/AuthContainer";
 import { Toaster } from "react-hot-toast";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage"
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
+      <RouterProvider router={router} />
       <AuthContainer />;
     </QueryClientProvider>
   );
