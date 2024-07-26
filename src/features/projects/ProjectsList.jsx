c165c317e6e2f9ee9d5b86056e134c0a96911f09
d.fathi <d.fathi@kian.digital>
2024-07-26 16:04:35 +0330

    create Project folder and ProjectList component in

---
+++ src/features/projects/ProjectsList.jsx
@@ -0,0 +1,32 @@
+import { MdEdit, MdDelete } from "react-icons/md";
+
+const ProjectsList = () => {
+  return (
+    <>
+      <h1 className="text-xl font-medium">Projects</h1>
+      <div className="space-y-5">
+        <Project />
+      </div>
+    </>
+  );
+};
+
+export default ProjectsList;
+
+const Project = ({}) => {
+  return (
+    <div className="bg-[#f3f5fa] p-5 rounded-2xl flex items-center justify-between gap-20 text-[rgb(93,92,92)] text-sm">
+      <h3 className="text-base font-medium flex-1">
+        Title of project by mohammad
+      </h3>
+      <div>Web Designe</div>
+      <div>1000$</div>
+      <div>Des 04,2019</div>
+      {/* <div>Freelancer</div> */}
+      <div className="flex items-center justify-center gap-5">
+        <MdEdit className="w-5 h-5" />
+        <MdDelete className="w-5 h-5" />
+      </div>
+    </div>
+  );
+};
--- src/pages/OwnerDashboardPage.jsx
+++ src/pages/OwnerDashboardPage.jsx
@@ -1,5 +1,11 @@
+import ProjectsList from "../features/projects/ProjectsList";
+
 const OwnerDashboardPage = () => {
-  return <div className="p-10 md:max-w-5xl ">owner dasboard</div>;
+  return (
+    <div className="p-10 md:max-w-5xl ">
+      <ProjectsList />
+    </div>
+  );
 };
 
 export default OwnerDashboardPage;
