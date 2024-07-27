9d998ce5df22e50c21033f4e29d2481315ef5d79
d.fathi <d.fathi@kian.digital>
2024-07-27 15:38:24 +0330

    use useOwnerProjects in ProjectList

--- src/features/projects/ProjectsList.jsx
+++ src/features/projects/ProjectsList.jsx
@@ -2,15 +2,31 @@ import { MdEdit, MdDelete } from "react-icons/md";
 import { useOwnerProjects } from "./useOwnerProjects";
 
 const ProjectsList = () => {
-  const { isError, isLoading, projects } = useOwnerProjects();
+  const { isLoading, projects } = useOwnerProjects();
+  console.log(isLoading, projects);
 
-  console.log(isError, isLoading, projects);
+  if (isLoading) return <p className="text-center">is loading...</p>;
+
+  if (!projects?.length)
+    return (
+      <p className="text-base text-slate-500 text-center">
+        ther is no project. create project
+      </p>
+    );
 
   return (
     <>
       <h1 className="text-xl font-medium">Projects</h1>
       <div className="space-y-5">
-        <Project />
+        {projects.map((project) => (
+          <Project
+            key={project._id}
+            title={project.title}
+            budget={project.budget}
+            category={project.category.title}
+            deadline={project.deadline}
+          />
+        ))}
       </div>
     </>
   );
@@ -18,15 +34,13 @@ const ProjectsList = () => {
 
 export default ProjectsList;
 
-const Project = ({}) => {
+const Project = ({ title, budget, category, deadline }) => {
   return (
     <div className="bg-[#f3f5fa] p-5 rounded-2xl flex items-center justify-between gap-20 text-[rgb(93,92,92)] text-sm">
-      <h3 className="text-base font-medium flex-1">
-        Title of project by mohammad
-      </h3>
-      <div>Web Designe</div>
-      <div>1000$</div>
-      <div>Des 04,2019</div>
+      <h3 className="text-base font-medium flex-1">{title}</h3>
+      <div>{category}</div>
+      <div>{budget}$</div>
+      <div>{deadline}</div>
       {/* <div>Freelancer</div> */}
       <div className="flex items-center justify-center gap-5">
         <MdEdit className="w-5 h-5" />
