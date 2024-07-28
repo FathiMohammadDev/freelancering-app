578f8fb49587625bed202683239d05d3b2dc54bd
d.fathi <d.fathi@kian.digital>
2024-07-28 17:26:05 +0330

    create deleteOwnerProjectApi service

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -4,3 +4,7 @@ import http from "./httpService"
 export const getOwnerProjectApi = () => {
     return http.get("/project/owner-projects").then(({ data }) => data.data)
 }
+
+export const deleteOwnerProjectApi = (id) => {
+    return http.delete(`/project/${id}`).then(({ data }) => data.data)
+}
