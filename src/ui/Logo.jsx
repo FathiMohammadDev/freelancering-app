922d352ef283c5bd74cfe3bf0780cedae020e221
d.fathi <d.fathi@kian.digital>
2024-08-03 19:34:19 +0330

    create Logo component

---
+++ src/ui/Logo.jsx
@@ -0,0 +1,12 @@
+import { FcOrgUnit } from "react-icons/fc";
+
+const Logo = () => {
+  return (
+    <div className="flex items-center justify-center gap-5">
+      <h1 className="text-primary text-2xl font-bold">LOGO.</h1>
+      <FcOrgUnit className="w-10 h-10" />
+    </div>
+  );
+};
+
+export default Logo;
