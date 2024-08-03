4693a28b2b1ac2faa97d4e162239c7b1dd3d95e1
d.fathi <d.fathi@kian.digital>
2024-08-03 15:31:26 +0330

    use components and handle get data

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useOwnerProject } from "../features/project/useOwnerProject";
+import ProposalList from "../features/project/ProposalList";
 
 const ProjectPage = () => {
-  return <div>ProjectPage</div>;
+  const { isLoading, project, isError } = useOwnerProject();
+
+  if (isLoading) return <p className="text-center">is loading...</p>;
+
+  if (!project)
+    return (
+      <p className="text-base text-slate-500 text-center">
+        ther is no project. create project
+      </p>
+    );
+
+  return (
+    <div className="p-10 md:max-w-5xl ">
+      <div>
+        <h1>{project.title}</h1>
+        <p>{project.description}</p>
+      </div>
+      <ProposalList proposals={project.proposals} />
+    </div>
+  );
 };
 
 export default ProjectPage;
