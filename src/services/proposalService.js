ada43e800d45d6fc49f214fca0ba2b4ba6c2f07e
d.fathi <d.fathi@kian.digital>
2024-08-07 17:25:39 +0330

    create getProposalsApi service

--- src/services/proposalService.js
+++ src/services/proposalService.js
@@ -4,3 +4,7 @@ import http from "./httpService"
 export const changePropsalStatusApi = ({ id, values }) => {
     return http.patch(`proposal/${id}`, values).then(({ data }) => data.data)
 }
+
+export const getProposalsApi = () => {
+    return http.get("proposal/list").then(({ data }) => data.data)
+}
