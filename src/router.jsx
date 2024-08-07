f6d6719cc605f4634eaf92bd5413e4ef1355b302
d.fathi <d.fathi@kian.digital>
2024-08-07 17:26:04 +0330

    create primitiv freelancer routes

--- src/router.jsx
+++ src/router.jsx
@@ -7,6 +7,8 @@ import OwnerLayout from "./layouts/OwnerLayout";
 import OwnerDashboardPage from "./pages/OwnerDashboardPage";
 import ProjectPage from "./pages/ProjectPage";
 import ProjectsPage from "./pages/ProjectsPage";
+import FreelancerLayout from "./layouts/FreelancerLayout";
+import FreelancerDashboard from "./pages/FreelancerDashboard";
 
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
@@ -20,6 +22,16 @@ export const router = createBrowserRouter([
       { path: "dashboard", element: <OwnerDashboardPage /> },
       { path: "projects", element: <ProjectsPage /> },
       { path: "projects/:id", element: <ProjectPage /> },
+    ],
+  },
+  {
+    path: "/freelancer",
+    element: <FreelancerLayout />,
+    children: [
+      { index: true, element: <Navigate to="dashboard" /> },
+      { path: "dashboard", element: <FreelancerDashboard /> },
+      { path: "projects", element: <Navigate to="dashboard" /> },
+      { path: "proposals", element: <Navigate to="dashboard" /> },
     ],
   },
   { path: "*", element: <NotFoundPage /> },
