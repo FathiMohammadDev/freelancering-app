43154dc1109940b987f22c67e6e8e3bdbf0f9358
d.fathi <d.fathi@kian.digital>
2024-08-08 16:40:27 +0330

    create CreateProposalForm and handle add proposal

--- src/features/freelancer/projects/Project.jsx
+++ src/features/freelancer/projects/Project.jsx
@@ -1,33 +1,52 @@
 import { toLocalDate } from "../../../utils/toLocalDate";
+import Modal from "../../../ui/Modal";
+import CreateProposalForm from "../../proposals/CreateProposalForm";
+import { useState } from "react";
 
 const Project = ({ project }) => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="bg-bg_primary rounded-2xl shadow-sm p-5 space-y-5 border border-border">
-      <div className="flex items-start justify-between">
-        <h1 className="text-lg font-bold text-text_primary">{project.title}</h1>
-        <div className="space-y-2">
-          <div className="text-text_primary font-bold text-sm capitalize">
-            {project.category.title}
-          </div>
-          <div className="text-xs text-text_secondary text-right">
-            {toLocalDate(project.createdAt)}
+    <>
+      <div className="bg-bg_primary rounded-2xl shadow-sm p-5 space-y-5 border border-border">
+        <div className="flex items-start justify-between">
+          <h1 className="text-lg font-bold text-text_primary">
+            {project.title}
+          </h1>
+          <div className="space-y-2">
+            <div className="text-text_primary font-bold text-sm capitalize">
+              {project.category.title}
+            </div>
+            <div className="text-xs text-text_secondary text-right">
+              {toLocalDate(project.createdAt)}
+            </div>
           </div>
         </div>
+        <div>
+          <p className="text-sm text-text_secondary leading-[25px] md:max-w-xl ">
+            {project.description}
+          </p>
+        </div>
+        <div className="space-x-4">
+          {project.tags.map((tag) => (
+            <span className="bg-bg_active capitalize text-sm text-text_secondary py-1 px-3 border border-border rounded-2xl">
+              {tag}
+            </span>
+          ))}
+        </div>
+        <button
+          onClick={() => setOpen(!open)}
+          className="btn bg-primary text-white"
+        >
+          Send proposal
+        </button>
       </div>
-      <div>
-        <p className="text-sm text-text_secondary leading-[25px] md:max-w-xl ">
-          {project.description}
-        </p>
-      </div>
-      <div className="space-x-4">
-        {project.tags.map((tag) => (
-          <span className="bg-bg_active capitalize text-sm text-text_secondary py-1 px-3 border border-border rounded-2xl">
-            {tag}
-          </span>
-        ))}
-      </div>
-      <button className="btn bg-primary text-white">Send Proposal</button>
-    </div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <CreateProposalForm
+          projectId={project._id}
+          onClose={() => setOpen(false)}
+        />
+      </Modal>
+    </>
   );
 };
 
---
+++ src/features/proposals/CreateProposalForm.jsx
@@ -0,0 +1,67 @@
+import { useForm } from "react-hook-form";
+import { useCreateProposal } from "./useCreateProposal";
+import TextField from "../../ui/TextField"
+
+const CreateProposalForm = ({ projectId,onClose }) => {
+  const { isPending, mutate } = useCreateProposal();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const newPropValue = { ...data, projectId };
+    mutate(newPropValue, {
+      onSuccess: () => {
+        onClose();
+        reset();
+      },
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        register={register}
+        errors={errors}
+        validationschema={{
+          required: "this field required",
+          minLength: {
+            value: 11,
+            message: "should be 11 character",
+          },
+        }}
+        name="description"
+      />
+      <TextField
+        register={register}
+        errors={errors}
+        validationschema={{
+          required: "this field required",
+          pattern: {
+            value: /[0-9]/,
+            message: "please enter number",
+          },
+        }}
+        name="duration"
+      />
+      <TextField
+        register={register}
+        errors={errors}
+        validationschema={{
+          required: "this field required",
+          pattern: {
+            value: /[0-9]/,
+            message: "please enter number",
+          },
+        }}
+        name="price"
+      />
+      <button className="form-btn">Edit project</button>
+    </form>
+  );
+};
+
+export default CreateProposalForm;
