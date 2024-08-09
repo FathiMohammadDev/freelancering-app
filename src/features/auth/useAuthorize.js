3e1fdc1cb504662f82e47f7ada9245807d5779bd
d.fathi <d.fathi@kian.digital>
2024-08-10 00:03:27 +0330

    create useAuthorize hook

---
+++ src/features/auth/useAuthorize.js
@@ -0,0 +1,22 @@
+import { useLocation } from "react-router-dom"
+import useUser from "./useUser"
+
+export const useAuthorize = () => {
+    const { isLoading, user } = useUser()
+    const { pathname } = useLocation()
+    const desierdRole = pathname.split("/").at(1)
+    let isAuthorized = false
+    let isAuthenticated = false
+    if (user) isAuthenticated = true
+    const ROLES = {
+        adimn: "ADMIN",
+        owner: "OWNER",
+        freelancer: "FREELANCER",
+    }
+
+    if (Object.keys(ROLES).includes(desierdRole)) {
+        if (user && user.role === ROLES[desierdRole]) isAuthorized = true
+    }
+
+    return { isLoading, isAuthenticated, isAuthorized, user }
+}
