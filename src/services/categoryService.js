e0a393383782bee538e0885c18ddc52a576a0eb7
d.fathi <d.fathi@kian.digital>
2024-08-12 22:51:11 +0330

    create createCategoriApi and removeCategoriApi service

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -3,3 +3,9 @@ import http from "./httpService"
 export const getCategoryListApi = () => {
     return http.get("/category/list").then(({ data }) => data.data)
 }
+export const createCategoriApi = (data) => {
+    return http.post("/admin/category/add", data).then(({ data }) => data.data)
+}
+export const removeCategoriApi = (id) => {
+    return http.delete(`/admin/category/remove/${id}`).then(({ data }) => data.data)
+}
