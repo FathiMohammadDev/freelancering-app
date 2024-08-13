37542ddb769934bcaede8fe08a38f4b302dd1e1b
d.fathi <d.fathi@kian.digital>
2024-08-13 17:09:41 +0330

    change openSidebar icon

--- src/layouts/FreelancerLayout.jsx
+++ src/layouts/FreelancerLayout.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router-dom";
-import OwnerSidebar from "./OwnerSidebar";
 import { useState } from "react";
 import DarkMode from "../ui/DarkMode";
 import User from "../features/auth/User";
 import FreelancerSidebar from "./FreelancerSidebar";
+import { HiMenuAlt2 } from "react-icons/hi";
 
 const FreelancerLayout = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
@@ -15,11 +15,11 @@ const FreelancerLayout = () => {
       />
       <div className="md:ml-[265px] min-h-screen">
         <div className="bg-bg_primary p-3 flex items-center gap-4 sticky top-0">
+          <div className="md:hidden" onClick={() => setOpenSidebar(true)}>
+            <HiMenuAlt2 className="w-7 h-7 cursor-pointer text-text_secondary" />
+          </div>
           <DarkMode />
           <User />
-          <div className="md:hidden" onClick={() => setOpenSidebar(true)}>
-            show
-          </div>
         </div>
         <Outlet />
       </div>
