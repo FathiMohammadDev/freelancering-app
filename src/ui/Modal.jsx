14f6aef8c6aded7584b0d83bc877480693af5f42
d.fathi <d.fathi@kian.digital>
2024-08-18 02:29:40 +0330

    change modal styles

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -2,27 +2,29 @@ import { useRef } from "react";
 import { MdOutlineClose } from "react-icons/md";
 import { useOutsideClick } from "../hooks/useOutsideClick";
 
-const Modal = ({ children, open, onClose }) => {
+const Modal = ({ children, open, onClose, title = "Title for modal" }) => {
   const ref = useRef();
 
   useOutsideClick(ref, open, onClose);
 
   const openClass =
-    "fixed z-50 top-0 left-0 w-full -translate-y-0 h-screen bg-bg_modal backdrop-blur-sm transition-all";
+    "fixed z-50 top-0 left-0 w-full -translate-y-0 h-screen bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 bg-black transition-all";
   const closeClass =
-    "fixed z-50 top-0 left-0 w-full -translate-y-full h-screen bg-bg_modal backdrop-blur-sm transition-all";
+    "fixed z-50 top-0 left-0 w-full -translate-y-full h-screen bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 bg-black transition-all";
   return (
     <div className={`${open ? openClass : closeClass}`}>
       <div
         ref={ref}
-        className="bg-bg_primary rounded-2xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-sm w-full sm:max-w-md"
+        className="bg-bg_primary space-y-4 rounded-2xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-8 shadow-sm w-full sm:max-w-xl"
       >
-        <div className="flex justify-end">
+        <div className="flex items-center justify-between">
+        <h3 className="text-base font-medium">{title}</h3>
           <MdOutlineClose
-            className="w-6 h-6 text-black cursor-pointer"
+            className="w-5 h-5 text-text_primary cursor-pointer"
             onClick={onClose}
           />
         </div>
+        <hr className="bg-border" />
         {children}
       </div>
     </div>
