af6ec243acef4e26a89de2d4f191b9e62881a5dc
d.fathi <d.fathi@kian.digital>
2024-08-20 23:19:02 +0330

    install and launch testing library

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+    globals: true,
+  },
+  server: {
+    port: 3000
   }
 })
